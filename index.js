d04be1a8a3604b4697263d2f1b7c910f66e55342
pajamaw <pjpeterww@gmail.com>
2016-10-03 19:11:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much.";
+
+const digitalClock = {
+  time: Math.round(Date.now()/1000),
+  startTicking: function(){
+    setInterval(() => this.time++, 1000);
+  }
+}
+
+function censor(word, string){
+  const matched = new RegExp(word, 'gi')
+  return string.replace(matched, 'BLEEP')
+}
+
+const violenceCensor = censor.bind(null, 'violence')
+const drugsCensor = censor.bind(null, 'drugs')
